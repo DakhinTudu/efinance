21fec03fa67730b55564f753359efb56f94159b5
DakhinTudu <107021203+DakhinTudu@users.noreply.github.com>
2026-04-06 15:39:23 +0000

    Deploying to gh-pages from @ DakhinTudu/efinance@03998547d2b4604e15353571f048a321b3f141ed 🚀

--- js/config.js
+++ js/config.js
@@ -3,7 +3,6 @@
 window.env = {
     // Current Local Backend (Development)
     API_BASE: 'http://localhost:8080/api/v1',
-    
-    // Example: Productive Backend (Uncomment and replace with your hosted URL)
-    // API_BASE: 'https://your-backend-api.onrender.com/api/v1'
+    // Active Backend
+    API_BASE: 'https://efinance-al0m.onrender.com/api/v1'
 };
